287727ced83f4f7e15aca31e61bbde304d2264bd
Sebastian Krajewski <mail@wieku.me>
2022-12-03 22:48:32 +0100

    Upload day 3

--- 2022/day03/main.go
+++ 2022/day03/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"aoc2021/util"
 	"fmt"
+	"strings"
 )
 
 var lines = util.ReadLines("2022/day03/input.txt")
@@ -13,9 +14,49 @@ func main() {
 }
 
 func p1() int {
-	return 0
+	sum := 0
+
+	for _, line := range lines {
+		ln := len(line) / 2
+
+		left, right := line[:ln], line[ln:]
+
+		for j := 0; j < len(left); j++ {
+			cChar := left[j]
+
+			if strings.IndexByte(right, cChar) > -1 {
+				sum += getScore(cChar)
+				break
+			}
+		}
+	}
+
+	return sum
 }
 
 func p2() int {
-	return 0
+	sum := 0
+
+	for i := 0; i < len(lines); i += 3 {
+		first, second, third := lines[i], lines[i+1], lines[i+2]
+
+		for j := 0; j < len(first); j++ {
+			cChar := first[j]
+
+			if strings.IndexByte(second, cChar) > -1 && strings.IndexByte(third, cChar) > -1 {
+				sum += getScore(cChar)
+				break
+			}
+		}
+	}
+
+	return sum
+}
+
+func getScore(k uint8) int {
+	if k >= 'a' {
+		return int(k - 'a' + 1)
+	}
+
+	return int(k - 'A' + 27)
 }
