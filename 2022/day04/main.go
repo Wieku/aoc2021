2b8da40c430e383421186702941d2d081dfa2ff7
Sebastian Krajewski <mail@wieku.me>
2022-12-04 16:56:48 +0100

    Upload day 4

--- 2022/day04/main.go
+++ 2022/day04/main.go
@@ -3,9 +3,35 @@ package main
 import (
 	"aoc2021/util"
 	"fmt"
+	"strings"
 )
 
 var lines = util.ReadLines("2022/day04/input.txt")
+
+type pair struct {
+	firstLower int
+	firstUpper int
+
+	secondLower int
+	secondUpper int
+}
+
+var pairs []*pair
+
+func init() {
+	for _, line := range lines {
+		spl := strings.Split(line, ",")
+		first := strings.Split(spl[0], "-")
+		second := strings.Split(spl[1], "-")
+
+		pairs = append(pairs, &pair{
+			firstLower:  util.Atoi(first[0]),
+			firstUpper:  util.Atoi(first[1]),
+			secondLower: util.Atoi(second[0]),
+			secondUpper: util.Atoi(second[1]),
+		})
+	}
+}
 
 func main() {
 	fmt.Println("Part 1:", p1())
@@ -13,9 +39,25 @@ func main() {
 }
 
 func p1() int {
-	return 0
+	sum := 0
+
+	for _, p := range pairs {
+		if (p.secondLower <= p.firstLower && p.firstUpper <= p.secondUpper) || (p.firstLower <= p.secondLower && p.secondUpper <= p.firstUpper) {
+			sum++
+		}
+	}
+
+	return sum
 }
 
 func p2() int {
-	return 0
+	sum := 0
+
+	for _, p := range pairs {
+		if p.firstLower <= p.secondUpper && p.firstUpper >= p.secondLower {
+			sum++
+		}
+	}
+
+	return sum
 }
