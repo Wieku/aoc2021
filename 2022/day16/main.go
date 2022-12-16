e4244e989b4b469a51c27ae06ad1ab657098cd56
Sebastian Krajewski <mail@wieku.me>
2022-12-17 00:00:33 +0100

    sum code

--- 2022/day16/main.go
+++ 2022/day16/main.go
@@ -3,9 +3,42 @@ package main
 import (
 	"aoc2021/util"
 	"fmt"
+	"strings"
 )
 
 var lines = util.ReadLines("2022/day16/input.txt")
+
+type valve struct {
+	rate int
+	adj  []int
+}
+
+func parse() (int, []*valve) {
+	idMap := make(map[string]int)
+
+	var valves []*valve
+	var valvesAdj []string
+
+	for _, line := range lines {
+		var vName string
+		var v valve
+		var vAdj string
+
+		fmt.Sscanf(line, "Valve %s has flow rate=%d; tunnels lead to valves %s", &vName, &v.rate, &vAdj)
+
+		idMap[vName] = len(valves)
+		valves = append(valves, &v)
+		valvesAdj = append(valvesAdj, vAdj)
+	}
+
+	for i, vAdj := range valvesAdj {
+		for _, nV := range strings.Split(vAdj, ", ") {
+			valves[i].adj = append(valves[i].adj, idMap[nV])
+		}
+	}
+
+	return idMap["AA"], valves
+}
 
 func main() {
 	fmt.Println("Part 1:", p1())
@@ -13,7 +46,12 @@ func main() {
 }
 
 func p1() int {
+
 	return 0
+}
+
+func passValve(vNum, timeLeft int, open bool, opened uint64, valves []*valve) int {
+
 }
 
 func p2() int {
